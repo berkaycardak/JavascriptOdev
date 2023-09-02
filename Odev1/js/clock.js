856bb81822a721e26fd4b545b85cc9400b053ee8
berkayc <berkay.cardak@hotmail.com>
2023-09-02 03:27:19 +0300

    first commit. I try something

---
+++ Odev1/js/clock.js
@@ -0,0 +1,22 @@
+function showTime() {
+    const weekday = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
+   let d = new Date()
+   let h = d.getHours()
+   let m = d.getMinutes()
+   let s = d.getSeconds()
+   let day = weekday[d.getDay()]
+   let myClock = document.querySelector('#myClock')
+   myClock.innerHTML=`${h}:${m}:${s} ${day}`
+} 
+
+setInterval(showTime, 1000);
+
+
+let fullName = prompt("Adınız Nedir?")
+let myName = document.querySelector('#myName')
+myName.innerHTML= fullName
+
+if(!fullName){
+    const element = document.getElementById("body")
+    element.remove();
+}
